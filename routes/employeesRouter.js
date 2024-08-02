1890246f5eae29c198940da9752d2405b09575de
TomBer101 <tomerm1233@gmail.com>
2024-08-02 19:12:39 +0300

    employee router - aded middlawares to 'delete employee'

--- routes/employeesRouter.js
+++ routes/employeesRouter.js
@@ -10,8 +10,8 @@ const loggerMiddlware = require('../middlewares/logMiddleware');
 
 //TODO: i thing this entire rout needs to use these two middlwares
 router.get('/', authMiddleware, allowActionsMiddleware, loggerMiddlware, employeesController.getAllEmployees);
-router.patch('/:employeeId', authMiddleware, allowActionsMiddleware, employeesController.updateEmployee)
-router.post('/', authMiddleware,actionMiddleware, employeesController.addEmployee)
-router.delete('/:employeeId', employeesController.deleteEmployee)
+router.patch('/:employeeId', authMiddleware, allowActionsMiddleware, loggerMiddlware, employeesController.updateEmployee)
+router.post('/', authMiddleware,actionMiddleware, loggerMiddlware, employeesController.addEmployee)
+router.delete('/:employeeId', authMiddleware,actionMiddleware, loggerMiddlware, employeesController.deleteEmployee)
 
 module.exports = router;
